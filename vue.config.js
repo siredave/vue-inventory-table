8eb2346c9683a155748eb995c88ebe05d58fd293
David <siredave1@gmail.com>
2025-01-17 20:13:56 +0100

    made some changes

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-inventory-table/' : '/'
-};
+    publicPath: process.env.NODE_ENV === 'production' ? '/vue-inventory-table/' : '/'
+  };
+  
